417d16f06e07bda0fbfead8aa0f636e19f579a2a
Felipe <felipevalerioderamos@gmail.com>
2024-09-06 15:14:19 -0300

    creating select stament

--- src/node.rs
+++ src/node.rs
@@ -1,6 +1,25 @@
 pub enum Node {
-	Select,
+	Select{
+		columns: Vec<String>,
+		table_name: String,
+	},
 	Insert,
 	Update,
 	Delete,
+}
+
+
+pub fn select_statement() {
+
+	if(tokenized_query.key == "KEYWORD" && tokenized_query.value == "SELECT") {
+
+		let mut columns = Vec::new();
+
+		if(tokenized_query.)
+	}
+	else {
+		eprintln!("Expected for SELECT keyword");
+		process::exit(1);
+	}
+
 }
