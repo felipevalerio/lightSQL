eac6f410b6a04e841eb81566aa5f9d8f7fd54265
Felipe <felipevalerioderamos@gmail.com>
2024-09-05 23:01:38 -0300

    doing some changes

--- src/node.rs
+++ src/node.rs
@@ -1,12 +1,6 @@
 pub enum Node {
-	Select {
-		columns: Vec<String>,
-		from_clause: String,
-		where_clause: Option<Box<Node>>,
-	},
-	Insert {
-		columns: Vec<String>,
-		into_clause: String,
-		
-	}
+	Select,
+	Insert,
+	Update,
+	Delete,
 }
