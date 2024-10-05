6851d6e594bc8b283787869284d606e25a99988f
Felipe <felipevalerioderamos@gmail.com>
2024-10-04 23:32:16 -0300

    making some changes in the node file

--- src/node.rs
+++ src/node.rs
@@ -1,34 +1,10 @@
+mod token_type;
+
 pub enum Node {
-	Select{
-		columns: Vec<String>,
-		table_name: String,
-	},
-	Insert,
-	Update,
-	Delete,
+
 }
 
 
 pub fn select_statement() -> Result<AstNode, String> {
 
-	let mut columns = Vec::new();
-	let mut table: String;
-
-	for (key, value) in &tokenized_query {
-		
-		if (key.contains_key("STRING")) {
-			columns.push(value);
-		}
-
-		if (key.contains_key("IDENTIFIER")) {
-			table = value;
-		}
-
-		if (key.contains_key("SYMBOL") ** value == ";") {
-			return Ok(Node::Select {
-				columns,
-				table_name: table
-			})
-		}
-	}
 }
