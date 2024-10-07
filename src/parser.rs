89b31a1f7789323d643e07b04d04c6472de89b3f
Felipe <felipevalerioderamos@gmail.com>
2024-10-06 23:20:18 -0300

    still creating the statements

---
+++ src/parser.rs
@@ -0,0 +1,23 @@
+
+pub enum Node {
+
+}
+
+pub struct SelectStatement {
+
+	columns: Vec<String>,
+	table: String,
+	where_clause: Option<WhereClause>
+}
+
+struct FromClause {
+
+}
+
+struct TableName {
+
+}
+
+struct WhereClause {
+
+}
