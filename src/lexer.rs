1c74f23a6051130c90c7d2bc82ac4b70985bde5b
Felipe <felipevalerioderamos@gmail.com>
2025-04-21 20:03:05 -0300

    refactoring a bunch of stuff

--- src/lexer.rs
+++ src/lexer.rs
@@ -39,7 +39,6 @@ impl Lexer {
     // 		Verifica se é uma palavra-chave ("SELECT", "FROM")
     // 		Se não for, trata como identificador comum
 	// 		Se for outro caractere (não suportado), ignora e continua
-
 	pub fn next_token(&mut self) -> Option<TokenType> {
 		
 		self.skip_whitespace();
@@ -74,58 +73,46 @@ impl Lexer {
 	}
 
 
-	fn skip_whitespace(&mut self) {
-		// map_or tem um valor padrão "False", mas se o current_char for diferente de None, ele atribue o valor dado "True"
-        while self.current_char.map_or(false, char::is_whitespace) {
-            self.read_char();
-        }
-    }
+	fn read_identifier(&mut self) -> String {
 
-
-	fn read_char(&mut self) {
-
-		if self.read_position >= self.input.len() {
-			self.current_char = None;
-		} else {
-			self.current_char = self.input[self.read_position..].chars().next();
-		}
-
-		self.position = self.read_position;
-		self.read_position += 1;
-	}
-
-
-	fn read_number(&mut self) -> String {
-		
 		let start = self.position;
 
-		while let Some(c) = self.current_char {
-            if c.is_digit(10) {
-                self.read_char();
-            } else { break;}
-        }
+		while self.position < self.input.len() {
 
+			let current_char = self.input.chars().nth(self.position).unwrap();
+			if !current_char.is_alphabetic() {
+				break;
+			}
+			self.position += 1;
+		}
 		self.input[start..self.position].to_string()
 	}
 
-	// keywords primeiramente tem valores do tipo &str
-	// depois é feita a iteração nos valores (iter)
-	// transforma cada um dos valores e converte eles para String (map)
-	// e depois transforma em uma collections e o _ no HashSet
-	// pede para o compilador inferir o novo tipo do HashSet, que será String
-	fn lookup_keyword(&mut self, input: &str) -> TokenType {
 
-		let keywords: HashSet<_> = ["SELECT", "INSERT", "UPDATE", "DELETE", "FROM", "WHERE"]
-            .iter()
-            .map(|&s| s.to_string())
-            .collect();
+	fn skip_whitespace(&mut self) {
+		while self.position < self.input.len() {
 
-        if keywords.contains(&input.to_uppercase()) {
-            TokenType::new("KEYWORD", input)
-        } else {
-            TokenType::new("IDENTIFIER", input)
-        }
-	}
+			let current_char = self.input.chars().nth(self.position).unwrap();
+			if !current_char.is_whitespace() {
+				break;
+			}
+			self.position += 1;
+    }
+}
+
+
+	// fn read_number(&mut self) -> String {
+		
+	// 	let start = self.position;
+
+	// 	while let Some(c) = self.current_char {
+    //         if c.is_digit(10) {
+    //             self.read_char();
+    //         } else { break;}
+    //     }
+
+	// 	self.input[start..self.position].to_string()
+	// }
 
 
 	pub fn check_grammar(&mut self) {
@@ -172,35 +159,6 @@ impl Lexer {
 			}
 		}
 
-	}
-
-
-	fn read_identifier(&mut self) -> String {
-		let start = self.position;
-        while let Some(c) = self.current_char {
-            if c.is_alphanumeric() || c == '_' {
-                self.read_char();
-            } else {
-                break;
-            }
-        }
-        self.input[start..self.position].to_string()
-	}
-
-
-	fn read_string(&mut self) -> String {
-		self.read_char();
-        let start = self.position;
-        while let Some(c) = self.current_char {
-            if c != '\'' {
-                self.read_char();
-            } else {
-                break;
-            }
-        }
-        let result = self.input[start..self.position].to_string();
-        self.read_char();
-        result
 	}
 
 }
