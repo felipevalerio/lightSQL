5f9e5d677b2c3954f2fb176ef809ebddc294b884
Felipe <felipevalerioderamos@gmail.com>
2025-04-25 22:23:28 -0300

    implementing a parser

--- src/parser.rs
+++ src/parser.rs
@@ -5,5 +5,19 @@ pub struct Parser {
 
 impl Parser {
 
+	pub fn parse(&mut self) -> Result<Statement, String> {
+		self.parse_select_statement()
+	}
+
+	
 	
 }
+
+
+// Recebe os tokens do lexer
+
+// Percorre os tokens sequencialmente
+
+// Verifica se os tokens estão na ordem correta (ex: SELECT deve vir antes de FROM)
+
+// Constrói os nós da AST conforme reconhece partes da query
