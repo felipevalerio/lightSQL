45efd48e9856d37d7a77bc29bc246a925d596e33
Felipe <felipevalerioderamos@gmail.com>
2025-04-26 21:08:27 -0300

    implementing a parser

--- src/parser.rs
+++ src/parser.rs
@@ -10,6 +10,20 @@ impl Parser {
 	}
 
 	
+	fn parse_select_statement(&mut self) -> Result<Statement, String> {
+
+		self.expect(Token::SELECT)?; // Verifica se o primeiro token é SELECT
+		let columns = self.parse_columns()?; // Parseia a lista de colunas (ou *)
+		self.expect(Token::From)?; // Verifica se o próximo token é FROM
+
+		// Parseia o nome da tabela
+		let table = match self.next_token() {
+			Some(Token::Identifier(name)) => name,
+			_ => return Err("Expected table name after FROM".to_string()),
+		};
+
+		Ok(Statement::Select(SelectStatement { columns, table })) // Retorna a estrutura montada
+	}
 	
 }
 
